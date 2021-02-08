a767b66e4b8ce3c5922985690ad6b7bb0436a75b
duta2yh <78712720+duta2yh@users.noreply.github.com>
2021-02-08 11:57:11 +0800

    Update index.js

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ const tambahan = require("./lib/tambahan.js")
 const donasi = require("./lib/donasi.js");
 const info = require("./lib/info.js");
 /////////////////
-const BotName = 'REUZ BOT☄'; 
-const instagram = 'https://instagram.com/reuz.id'; 
+const BotName = 'RINI-BOT☄'; 
+const instagram = 'https://instagram.com/duta2005'; 
 const telegram = 'https://t.me/reuzzz'; 
-const kapanbotaktif = 'KETIKA HANYA DIAKTIFKAN'; 
-const youtube = 'https://www.youtube.com/c/reuz_id';
+const kapanbotaktif = '99999999999999JAM'; 
+const youtube = 'https://www.youtube.com/c/';
 //const grupch1 = 'belum ada grup'; 
 //const grupch2 = 'belum ada grup' ; 
 const
